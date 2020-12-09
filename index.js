1953211c0a4c957879442593e6876ba5a778740f
Vaisakhkm2625 <68694876+Vaisakhkm2625@users.noreply.github.com>
2020-12-09 16:41:40 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+let myShows = ['Bones', 'Psych', 'Big Bang Theory', 'Mad Men', 'Breaking Bad', 'Modern Family', 'Game of Thrones', 'Dexter'];
+let show = myShows[Math.floor(Math.random() * myShows.length)];
+alert(show)
